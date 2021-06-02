9d70e685948c69c3e93766f0dec0637400817e2d
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-06-02 10:59:23 +0100

    i think i'm beginning to understand this now

--- scalar_experiment.py
+++ scalar_experiment.py
@@ -7,16 +7,41 @@ from experts.problems import ScalarExpertsProblem
 import matplotlib.pyplot as plt
 
 # %%
-n_experts = 3
+n_experts = 10
 t_time = 100
-beta = 0.2
+beta = 0.01
+
+# This is probably what you would expect
+# when the experts and the outcome
+# are all random sequences
+# %%
+A = ScalarExpertsProblem(n_experts, t_time)
+result = A.mixture(beta)
 
 # %%
-A = ScalarExpertsProblem(n_experts, t_time, 1, 1, 1, 1, 1)
-result = A.mixture(beta)[0]
-
-# %%
-plt.plot(result)
+plt.plot(result[0])
 plt.show()
 
+# %%
+plt.plot(result[1])
+plt.show()
+
+# On the other hand,
+# when the outcome sequence
+# is one of the experts,
+# the learning algorithm
+# quickly follows the predictions
+# of that leading expert
+
+# %%
+A = ScalarExpertsProblem(n_experts, t_time, outcomeAsExpert=1)
+result = A.mixture(beta)
+
+# %%
+plt.plot(result[0])
+plt.show()
+
+# %%
+plt.plot(result[1])
+plt.show()
 # %%
