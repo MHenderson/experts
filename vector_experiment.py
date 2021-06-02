f94e64535055267fdd9fd52db6bf54716170c151
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-06-02 12:45:04 +0100

    update the vector_experiment

--- vector_experiment.py
+++ vector_experiment.py
@@ -1,12 +1,23 @@
+# %%
 import experts
+
 from experts import problems
+from experts.problems import VectorExpertsProblem
 
-vectorLength = 15
-noOfExperts = 20
-totalTime = 25
+import matplotlib.pyplot as plt
 
-A = experts.problems.VectorExpertsProblem(vectorLength, noOfExperts, totalTime, 1, 1, 1, 1)
-
+# %%
+vector_length = 15
+n_experts = 20
+t_time = 25
 beta = 0.1
 
+# %%
+A = VectorExpertsProblem(vector_length, n_experts, t_time, 1, 1, 1, 1)
+
 result = A.mixture(beta)
+
+# %%
+plt.plot(result[0])
+plt.show()
+# %%
