237bfbe1e5d07605b8c985ca1712ea1e97a9fa0c
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-06-02 10:03:36 +0100

    remove unused function

--- experts/utils.py
+++ experts/utils.py
@@ -5,10 +5,6 @@ def twoNorm(vector):
     length = vector.shape[0]
     return np.sqrt(sum(vector**2)/length)
 
-def oneNorm(vector):
-    length = vector.shape[0]
-    return np.sqrt(sum(vector**1)/length)
-
 def accumulate(vector):
     result = copy.deepcopy(vector)
     for i in range(len(vector)):
