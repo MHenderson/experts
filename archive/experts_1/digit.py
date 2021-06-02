75a19d0a8e4e81e0dc2f46b35f7a848bdb5b7887
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-06-02 07:57:01 +0100

    remove archive

--- archive/experts_1/digit.py
+++
@@ -1,8 +0,0 @@
-def digit(x,base,i):
-    return int((x/(base**i))%base)
-
-def allTuples(base,length):
-    a = range(base**length)
-    b = range(length)
-    b.reverse()
-    return [[digit(j,base,i) for i in b] for j in a]
--- archive/experts_1/graphTest.py
+++
@@ -1,7 +0,0 @@
-import graph
-
-vertices = range(2**3)
-edges = [[x,y] for x in vertices for y in vertices if x<>y and x^y in [2**i for i in range(3)]]
-adjacencies = dict(zip(vertices,[[y for y in vertices if [x,y] in edges] for x in vertices]))
-x = graph.Graph(vertices, edges, adjacencies)
-x.graphDraw()
