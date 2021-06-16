ad31d2b004c4d5b9212df666dc68b5be392e9641
Matthew Henderson <matthew.james.henderson@gmail.com>
2021-06-16 10:39:10 +0100

    first test, lol

--- tests/test_problems.py
+++ tests/test_problems.py
@@ -3,6 +3,7 @@ import pytest
 import experts
 from experts.problems import ScalarExpertsProblem
 
-def test_setting_name():
-    jack_account = "Matthew Henderson"
-    assert jack_account == "Matthew Henderson"
+def test_prediction_function():
+    A = ScalarExpertsProblem(1, 0)
+    x = A.predictionFunction(1, 0.1)
+    assert x == 1
